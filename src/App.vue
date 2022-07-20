865b022edeb70d913c461f1d1f9d87cb4d6c3a8c
Anuma Thomas <tombrownanuma@gmail.com>
2022-07-20 23:08:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <div class="title">My Blog</div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/join">Join Us</router-link>
+    </nav>
+  </header>
+  <router-view/>
+</template>
